d166baba824b925db0736c547859f6c38f5eb4ba
daniel-kzn <45941342+daniel-kzn@users.noreply.github.com>
2023-05-01 15:13:35 +0200

    bla

--- src/modules/authentication/authentication.guard.ts
+++ src/modules/authentication/authentication.guard.ts
@@ -30,7 +30,6 @@ export class AuthenticationGuard implements CanActivate {
         return true
 
         //
-        //
     }
 
     private extractTokenFromHeader(request: Request): string | undefined {
